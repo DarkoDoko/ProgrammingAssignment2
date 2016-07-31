c7d9d2b11162fc800e565013d844a39ba0fcd009
Darko Doko <darko.doko1@gmail.com>
2016-07-31 20:02:42 +0200

    Corrected names of variables and added comment with sample

--- cachematrix.R
+++ cachematrix.R
@@ -2,18 +2,19 @@
 ## In order to speed up the process we eill try caching the matrix inverse.
 
 ## This function creates special "matrix" object that can cache its inverse.
+
 makeCacheMatrix <- function(x = matrix()) {
     inverse <- NULL
 
     set <- function(y){
         x <<- y
-        m <<- NULL
+        inverse <<- NULL
     }
 
     get <- function() x
 
-    setInverse <- function(solve) m <<- solve
-    getInverse <- function() m
+    setInverse <- function(newInv) inverse <<- newInv
+    getInverse <- function() inverse
 
     list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 
@@ -23,17 +24,37 @@ makeCacheMatrix <- function(x = matrix()) {
 ##  This function computes the inverse of the special "matrix" returned by makeCacheMatrix
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-    m <- x$getInverse()
+    inv <- x$getInverse()
 
-    if(!is.null(m)){
+    if(!is.null(inv)){
         message("getting cached data")
-        return(m)
+        return(inv)
     }
 
     data <- x$get()
-    m <- solve(data)
-
-    x$setInverse(m)
-
-    m
+    inv <- solve(data)
+    
+    x$setInverse(inv)
+    inv
 }
+## Sample run:
+## > x = rbind(c(1, -1/4), c(-1/4, 1))
+## > m = makeCacheMatrix(x)
+## > m$get()
+##       [,1]  [,2]
+## [1,]  1.00 -0.25
+## [2,] -0.25  1.00
+
+## No cache in the first run
+## > cacheSolve(m)
+##           [,1]      [,2]
+## [1,] 1.0666667 0.2666667
+## [2,] 0.2666667 1.0666667
+
+## Retrieving from the cache in the second run
+## > cacheSolve(m)
+## getting cached data.
+##           [,1]      [,2]
+## [1,] 1.0666667 0.2666667
+## [2,] 0.2666667 1.0666667
+## > 
